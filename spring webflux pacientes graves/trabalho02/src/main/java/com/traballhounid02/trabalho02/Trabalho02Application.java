71e3aab8a89e13abe4509594535c614f3527151d
Thauanny <thauanny.ramos.124@ufrn.edu.br>
2022-06-30 12:23:35 -0300

    envio para o professor

--- spring webflux pacientes graves/trabalho02/src/main/java/com/traballhounid02/trabalho02/Trabalho02Application.java
+++ spring webflux pacientes graves/trabalho02/src/main/java/com/traballhounid02/trabalho02/Trabalho02Application.java
@@ -3,13 +3,14 @@ package com.traballhounid02.trabalho02;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableEurekaClient
 public class Trabalho02Application {
-
+	@LoadBalanced
 	public static void main(String[] args) {
 		SpringApplication.run(Trabalho02Application.class, args);
 	}
